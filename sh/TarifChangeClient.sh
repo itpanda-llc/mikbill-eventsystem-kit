ebd2eba300ecca90749d4a705317bf60dca0a083
Mikhail Pakhomov <61195488+itpanda-llc@users.noreply.github.com>
2021-04-01 15:19:12 +0500

    Delete sh directory

--- sh/TarifChangeClient.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ../scripts/ || exit
-#/usr/bin/php ./TarifChangeClientSMSNotice.php "$1" > /dev/null 2>&1
-/usr/bin/php ./TarifChangeClientSocNotice.php "$1" > /dev/null 2>&1
-/usr/bin/php ./TarifChangeDiscountRemove.php "$1" > /dev/null 2>&1
-/usr/bin/php ./TarifChangeRouterOSSessionRemove.php "$1" > /dev/null 2>&1
